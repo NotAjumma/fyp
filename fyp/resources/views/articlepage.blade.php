1c0b681b0bff8beb0782dc3255fb7340edf478e2
nadirah <nadirahros.liza@live.iium.edu.my>
2024-04-30 10:55:04 +0800

    updated

---
+++ fyp/resources/views/articlepage.blade.php
@@ -0,0 +1,14 @@
+@extends('navbar.leftbar')
+
+@section('title')
+    Article
+@endsection
+
+@section('header')
+<div class="wrapper d-flex align-items-stretch">
+    <div id="content" class="p-4 p-md-5 pt-5">
+        <h1 class="mb-4">List of Article on Cyber Attacks</h1>
+        </div>
+    </div>
+</div>
+@endsection
