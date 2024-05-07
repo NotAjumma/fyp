be803e6f1ffc9e32857ccd4f5fe14c6df10e6139
anihanz <167504678+anihanz@users.noreply.github.com>
2024-05-07 22:38:39 +0800

    updated
    This reverts commit 0db2efac044f9b47a243871c9a1e042a02ed9c7d.

--- fyp/routes/web.php
+++ fyp/routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
 use App\Http\Controllers\ChapterController;
-Use App\Http\Controllers\CourseController;
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UploadFileController;
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,20 +14,29 @@ use App\Http\Controllers\HomeController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-
 Route::get('/', function () {
-    return view('chapterpage');
+    return view('landingpage');
 });
 
-Auth::routes();
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+Route::get('/coursepage', [CourseController::class, 'index'])->name('coursepage');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/admindash', [AdminController::class, 'index'])->name('admindashboard');
 
-Auth::routes();
+Route::get('/uploadfile', [UploadFileController::class, 'upload'])->name('uploadfile');
+Route::post('/uploadfile', [UploadFileController::class, 'uploadPost'])->name('uploadfile.post');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+require __DIR__.'/auth.php';
