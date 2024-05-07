da4ca2a0537e3691113f3379255ec4f3f9ff9f04
anihanz <167504678+anihanz@users.noreply.github.com>
2024-05-07 23:07:12 +0800

    adminupdated

--- fyp/database/migrations/2024_05_01_073115_create_files_table.php
+++ fyp/database/migrations/2024_05_01_073115_create_files_table.php
@@ -13,16 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('files', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('username')->nullable();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
-            $table->enum('role',['admin','user'])->default('user');
         });
     }
 
@@ -33,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('files');
     }
 };
